5ffca42050125aff8fc892fb253fc0f86e03d69c
yangSQI <3230278497@qq.com>
2018-06-05 12:15:24 +0800

    'test'

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import FastClick from 'fastclick'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
+
+
+import 'swiper/dist/css/swiper.css'
+
+FastClick.attach(document.body);
+Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
