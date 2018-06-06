9190188955dc4319e7d5a86e6100d25ddce5b51b
yangSQI <3230278497@qq.com>
2018-06-06 16:54:29 +0800

    master

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import FastClick from 'fastclick'
+import 'script/font-size'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import FastClick from 'fastclick'
+import 'babel-polyfill'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 
+import BScroll from 'better-scroll'
 
-
+import 'style/reset.css'
+import 'style/border.css'
 import 'swiper/dist/css/swiper.css'
 
 FastClick.attach(document.body);
