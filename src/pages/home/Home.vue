be07b05afd44a73d13e337665135d4fee552d5dc
yangSQI <3230278497@qq.com>
2018-06-06 17:09:56 +0800

    concise

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="container">
         <h1 class="page-header">el psy congroo!</h1>
+        <router-link to="/list">list组件</router-link>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Home"
+        name: "Home",
     }
 </script>
 
-<style lang="stylus" scoped>
-    .container
-        border: 1px solid #f40
-        .page-header
-            color: yellow
+<style scoped>
+
 </style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>List</h1>
+        <router-link to="/">首页组件</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "List",
+    }
+</script>
+
+<style scoped>
+
+</style>
